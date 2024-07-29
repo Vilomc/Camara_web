ad91f1189ee4d88f90c94a91d384dd1f9d52d45d
Vilomc <vilomc994@gmail.com>
2024-07-29 00:17:55 -0300

    aumentado calidad a snapshot

--- public/assets/js/handlerSnapshotWeb.js
+++ public/assets/js/handlerSnapshotWeb.js
@@ -1,18 +1,22 @@
 function takeSnapshot() {
-    const canvas = document.createElement('canvas');
-    const context = canvas.getContext('2d');
     const video = document.querySelector('img');
 
     if (!video) {
         console.error('Video element not found');
         return;
     }
+    const videoWidth = video.naturalWidth || video.width;
+    const videoHeight = video.naturalHeight || video.height;
 
-    canvas.width = video.width;
-    canvas.height = video.height;
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    const canvas = document.createElement('canvas');
+    canvas.width = videoWidth;
+    canvas.height = videoHeight;
 
-    const dataURL = canvas.toDataURL('image/jpeg');
+    const context = canvas.getContext('2d');
+    context.drawImage(video, 0, 0, videoWidth, videoHeight);
+
+    const dataURL = canvas.toDataURL('image/jpeg', 1.0); 
+
     const a = document.createElement('a');
     a.href = dataURL;
     a.download = 'snapshot.jpg';
