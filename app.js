99ea8dcaabffcf167647b8e49f9f4efe1ac4c482
Vilomc <vilomc994@gmail.com>
2024-07-28 21:33:35 -0300

    html + css + assets

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ import express from "express";
 import { config } from "dotenv";
 import { handlerCamara } from './handlerCamara.js';
 import { device } from "./handlerJoystick.js";
+import { fileURLToPath } from 'url';
+import path from "path";
 config();
 const app = express();
 app.use(express.json());
 const port = process.env.PORT ?? 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+//inicializamos endpoint
 app.get('/movecam', async (req, res) => {
     const { command, steps } = req.query;
         if (!command || !steps) {
@@ -21,6 +26,13 @@ app.get('/movecam', async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
+//inicializamos frontend
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public', 'index.html'));
+});
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
