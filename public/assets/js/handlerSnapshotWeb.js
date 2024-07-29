aa42047360243b8a6bf796a1e52a4e68cf1f93c8
Vilomc <vilomc994@gmail.com>
2024-07-29 00:14:43 -0300

    completado

--- public/assets/js/handlerSnapshotWeb.js
+++ public/assets/js/handlerSnapshotWeb.js
@@ -1,9 +1,16 @@
 function takeSnapshot() {
-    const canvas = document.getElementById('video-canvas');
-    if (!canvas) {
-        console.error('Canvas element not found');
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    const video = document.querySelector('img');
+
+    if (!video) {
+        console.error('Video element not found');
         return;
     }
+
+    canvas.width = video.width;
+    canvas.height = video.height;
+    context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
     const dataURL = canvas.toDataURL('image/jpeg');
     const a = document.createElement('a');
